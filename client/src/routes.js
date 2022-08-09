e7a9e2f29f59f0ef202f99bd6d9fe2871fc8341f
DevUreak <rlaquddlf3204@gmail.com>
2022-08-09 18:43:34 +0900

    client theme update

---
+++ client/src/routes.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+// Layout Types
+import { DefaultLayout } from "./layouts";
+
+// Route Views
+import BlogOverview from "./views/BlogOverview";
+import UserProfileLite from "./views/UserProfileLite";
+import AddNewPost from "./views/AddNewPost";
+import Errors from "./views/Errors";
+import ComponentsOverview from "./views/ComponentsOverview";
+import Tables from "./views/Tables";
+import BlogPosts from "./views/BlogPosts";
+import test1 from "./views/NFT_TEST_PAGE1";
+import test2 from "./views/NFT_TEST_PAGE2";
+
+export default [
+  {
+    path: "/",
+    exact: true,
+    layout: DefaultLayout,
+    component: () => <Redirect to="/blog-overview" />
+  },
+  {
+    path: "/blog-overview",
+    layout: DefaultLayout,
+    component: BlogOverview
+  },
+  {
+    path: "/user-profile-lite",
+    layout: DefaultLayout,
+    component: UserProfileLite
+  },
+  {
+    path: "/add-new-post",
+    layout: DefaultLayout,
+    component: AddNewPost
+  },
+  {
+    path: "/errors",
+    layout: DefaultLayout,
+    component: Errors
+  },
+  {
+    path: "/components-overview",
+    layout: DefaultLayout,
+    component: ComponentsOverview
+  },
+  {
+    path: "/tables",
+    layout: DefaultLayout,
+    component: Tables
+  },
+  {
+    path: "/blog-posts",
+    layout: DefaultLayout,
+    component: BlogPosts
+  },
+  {
+    path: "/test1",
+    layout: DefaultLayout,
+    component: test1
+  },
+  {
+    path: "/test2",
+    layout: DefaultLayout,
+    component: test2
+  }
+];
